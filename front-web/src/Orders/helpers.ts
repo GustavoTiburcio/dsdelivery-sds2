3437509ef567472c56b581a2dbd7cd3f24ea3c93
GustavoTiburcio <gustavotiburcio23@gmail.com>
2021-01-09 00:25:45 -0300

    Add env var API_URL

--- front-web/src/Orders/helpers.ts
+++ front-web/src/Orders/helpers.ts
@@ -3,7 +3,7 @@ import { Product } from "./types";
 export function checkIsSelected(selectedProducts: Product[], product: Product){
     return selectedProducts.some(item => item.id === product.id);
 }
-
+// test
 export function formatPrice(price: number){
     const formatter = new Intl.NumberFormat('pt-BR', {
        style: 'currency',
